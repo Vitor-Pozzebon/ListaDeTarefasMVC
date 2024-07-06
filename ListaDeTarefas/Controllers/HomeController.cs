2a3ea2260efce09bd8822809fe0a6f6863720f7d
Vitor Pozzebon <vitor.scalari@gmail.com>
2024-07-05 21:34:27 -0300

    Criando a segunda parte da tabela

--- ListaDeTarefas/Controllers/HomeController.cs
+++ ListaDeTarefas/Controllers/HomeController.cs
@@ -63,5 +63,64 @@ namespace ListaDeTarefas.Controllers
             return View(tarefas);
         }
 
+        public IActionResult Adicionar()
+        {
+            ViewBag.Categorias = _context.Categorias.ToList();
+            ViewBag.Status = _context.Statuses.ToList();
+
+            var tarefa = new Tarefa { StatusId = "aberto" };
+            return View(tarefa);
+        }
+
+        [HttpPost]
+        public IActionResult Filtrar(string[] filtro)
+        {
+            string id = string.Join('-', filtro);
+            return RedirectToAction("Index", new {ID = id});
+        }
+
+        [HttpPost]
+        public IActionResult MarcarCompleto([FromRoute] string id, Tarefa tarefaSelecionada)
+        {
+            tarefaSelecionada = _context.Tarefas.Find(tarefaSelecionada.Id);
+            if (tarefaSelecionada != null)
+            {
+                tarefaSelecionada.StatusId = "completo";
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index", new { ID = id });
+        }
+
+        [HttpPost]
+        public IActionResult Adicionar(Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tarefas.Add(tarefa);
+                _context.SaveChanges();
+                return RedirectToAction("Index"); 
+            }
+            else
+            {
+                ViewBag.Categorias = _context.Categorias.ToList();
+                ViewBag.Status = _context.Statuses.ToList();
+
+                return View(tarefa);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult DeletarCompletos(string id)
+        {
+            var deletar = _context.Tarefas.Where(s => s.StatusId == "completo").ToList();
+            foreach(var tarefa in deletar)
+            {
+                _context.Tarefas.Remove(tarefa);
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", new {ID = id});
+        }
     }
 }
